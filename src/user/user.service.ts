674d4704459b87592ed00feeec4f8280ee876804
dawnfire05 <dawnfire05@naver.com>
2024-07-23 21:25:39 +0900

    refactor

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,41 +4,39 @@ import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
-    constructor(private prisma: PrismaService){}
+  constructor(private prisma: PrismaService) {}
 
-    //post
-    async createUser(data: Prisma.UserCreateInput): Promise<User>{
-        return this.prisma.user.create({
-            data,
-        });
-    }
+  //post
+  async createUser(data: Prisma.UserCreateInput): Promise<User> {
+    return this.prisma.user.create({
+      data,
+    });
+  }
 
-    //get
-    async getUsers(): Promise<User[]>{
-        return this.prisma.user.findMany();
-    }
+  //get
+  async getUsers(): Promise<User[]> {
+    return this.prisma.user.findMany();
+  }
 
-    //get
-    async getUserById(id: number): Promise<User | null>{
-        return this.prisma.user.findUnique({
-            where: {id},
-        });
-    }
+  //get
+  async getUserById(id: number): Promise<User | null> {
+    return this.prisma.user.findUnique({
+      where: { id },
+    });
+  }
 
-    //put
-    async updateUser(id: number, data: Prisma.UserUpdateInput): Promise<User>{
-        return this.prisma.user.update({
-            where:{id},
-            data,
-        });
-    }
+  //put
+  async updateUser(id: number, data: Prisma.UserUpdateInput): Promise<User> {
+    return this.prisma.user.update({
+      where: { id },
+      data,
+    });
+  }
 
-    //delete
-    async deleteUser(id: number): Promise<User>{
-        return this.prisma.user.delete({
-            where: {id},
-        });
-    }
-
-
+  //delete
+  async deleteUser(id: number): Promise<User> {
+    return this.prisma.user.delete({
+      where: { id },
+    });
+  }
 }
