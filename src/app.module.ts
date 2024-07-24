a20f28e86c10f385b525aae5eb741752772083a9
dawnfire05 <dawnfire05@naver.com>
2024-07-24 20:38:11 +0900

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { UserModule } from './user/user.module';
 import { ChatService } from './chat/chat.service';
 import { ChatController } from './chat/chat.controller';
 import { ChatModule } from './chat/chat.module';
+import { TestModule } from './test/test.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, ChatModule],
+  imports: [PrismaModule, UserModule, ChatModule, TestModule],
   providers: [ChatService],
   controllers: [ChatController],
 })
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,34 +1,47 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { UserService } from './user.service';
-import { Prisma, User as UserModel} from '@prisma/client';
+import { Prisma, User as UserModel } from '@prisma/client';
 
 @Controller('user')
 export class UserController {
-    constructor(private readonly userService: UserService){}
+  constructor(private readonly userService: UserService) {}
 
-    @Post()
-    async createUser(@Body() userData: Prisma.UserCreateInput): Promise<UserModel>{
-        return this.userService.createUser(userData);
-    }
+  @Post()
+  async createUser(
+    @Body() userData: Prisma.UserCreateInput,
+  ): Promise<UserModel> {
+    return this.userService.createUser(userData);
+  }
 
-    @Get()
-    async getUsers(): Promise<UserModel[]>{
-        return this.userService.getUsers();
-    }
+  @Get()
+  async getUsers(): Promise<UserModel[]> {
+    return this.userService.getUsers();
+  }
 
-    @Get(':id')
-    async getUserById(@Param('id', ParseIntPipe) id : number): Promise<UserModel>{
-        return this.userService.getUserById(id);
-    }
+  @Get(':id')
+  async getUserById(@Param('id', ParseIntPipe) id: number): Promise<UserModel> {
+    return this.userService.getUserById(id);
+  }
 
-    @Put(':id')
-    async updateUser(@Param('id', ParseIntPipe) id : number, @Body() userData : Prisma.UserUpdateInput) : Promise<UserModel>{
-        return this.userService.updateUser(id, userData);
-    }
+  @Put(':id')
+  async updateUser(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() userData: Prisma.UserUpdateInput,
+  ): Promise<UserModel> {
+    return this.userService.updateUser(id, userData);
+  }
 
-    @Delete(':id')
-    async deleteUser(@Param('id', ParseIntPipe) id: number): Promise<UserModel>{
-        return this.userService.deleteUser(id);
-    }
-
+  @Delete(':id')
+  async deleteUser(@Param('id', ParseIntPipe) id: number): Promise<UserModel> {
+    return this.userService.deleteUser(id);
+  }
 }
