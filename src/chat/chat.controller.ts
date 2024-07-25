ef004dd82d8bc77624598417c76f54382685ec03
dawnfire05 <dawnfire05@naver.com>
2024-07-25 11:27:52 +0900

    chore: formatting

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -4,10 +4,12 @@ import { Chat as ChatModel, Prisma } from '@prisma/client';
 
 @Controller('chat')
 export class ChatController {
-    constructor(private readonly chatService: ChatService){}
+  constructor(private readonly chatService: ChatService) {}
 
-    @Post()
-    async createChat(@Body() chatData: Prisma.ChatCreateInput): Promise<ChatModel>{
-        return this.chatService.createChat(chatData)
-    }
+  @Post()
+  async createChat(
+    @Body() chatData: Prisma.ChatCreateInput,
+  ): Promise<ChatModel> {
+    return this.chatService.createChat(chatData);
+  }
 }
